98e2eaf5faf5083d019db711fdde19bb941287e1
StalledStorm <112809386+StalledStorm@users.noreply.github.com>
2026-05-24 03:53:41 +0100

    Sector 1 Regions

--- worlds/metroidfusion/data/logic/regions/Sector1.py
+++ worlds/metroidfusion/data/logic/regions/Sector1.py
@@ -1,57 +1,217 @@
 from ..FusionRegion import FusionRegion
 
 
-class Sector1Hub(FusionRegion):
-    name = "Sector 1 Hub"
+class Animorphs(FusionRegion):
+    name = "Animorphs"
 
-class Sector1TubeRight(FusionRegion):
-    name = "Sector 1 Tube Right"
+class AnimorphsCache(FusionRegion):
+    name = "Animorphs Cache"
 
-class Sector1TubeLeft(FusionRegion):
-    name = "Sector 1 Tube Left"
+class Antechamber(FusionRegion):
+    name = "Antechamber"
 
-class Sector1Antechamber(FusionRegion):
-    name = "Sector 1 Antechamber"
+class AtmosphericStabilizerCentral(FusionRegion):
+    name = "Atmospheric Stabilizer Central"
 
-class Sector1FirstStabilizerZone(FusionRegion):
-    name = "Sector 1 First Stabilizer Zone"
+class AtmosphericStabilizerNortheast(FusionRegion):
+    name = "Atmospheric Stabilizer Northeast"
 
-class Sector1SecondStabilizerZone(FusionRegion):
-    name = "Sector 1 Second Stabilizer Zone"
+class AtmosphericStabilizerNorthwest(FusionRegion):
+    name = "Atmospheric Stabilizer Northwest"
 
-class Sector1ThirdStabilizerZone(FusionRegion):
-    name = "Sector 1 Third Stabilizer Zone"
+class AtmosphericStabilizerSoutheast(FusionRegion):
+    name = "Atmospheric Stabilizer Southeast"
 
-class Sector1FourthStabilizerZone(FusionRegion):
-    name = "Sector 1 Fourth Stabilizer Zone"
+class AtmosphericStabilizerSouthwest(FusionRegion):
+    name = "Atmospheric Stabilizer Southwest"
 
-class Sector1ChargeCoreZone(FusionRegion):
-    name = "Sector 1 Charge Core Zone"
+class CentralSaveRoom(FusionRegion):
+    name = "Central Save Room"
 
-class Sector1AfterChargeCoreZone(FusionRegion):
-    name = "Sector 1 After Charge Core Zone"
+class ChargeCoreArena(FusionRegion):
+    name = "Charge Core Arena"
 
-class Sector1TourianExit(FusionRegion):
-    name = "Sector 1 Tourian Exit"
+class ChargeCoreLowerAccess(FusionRegion):
+    name = "Charge Core Lower Access"
 
-class Sector1TourianHub(FusionRegion):
-    name = "Sector 1 Tourian Hub"
+class ChargeCoreUpperAccess(FusionRegion):
+    name = "Charge Core Upper Access"
 
-class Sector1TourianHubElevatorTop(FusionRegion):
-    name = "Sector 1 Tourian Hub Elevator Top"
+class CrabRave(FusionRegion):
+    name = "Crab Rave"
+
+class EntranceLobby(FusionRegion):
+    name = "Entrance Lobby"
+
+class EntranceNavigationRoom(FusionRegion):
+    name = "Entrance Navigation Room"
+
+class EntranceRechargeRoom(FusionRegion):
+    name = "Entrance Recharge Room"
+
+class EntranceSaveRoom(FusionRegion):
+    name = "Entrance Save Room"
+
+class GenesisHabitation(FusionRegion):
+    name = "Genesis Habitation"
+
+class GoldPirateCrossing(FusionRegion):
+    name = "Gold Pirate Crossing"
+
+class HornoadHole(FusionRegion):
+    name = "Hornoad Hole"
+
+class HornoadHousing(FusionRegion):
+    name = "Hornoad Housing"
+
+class LavaLake(FusionRegion):
+    name = "Lava Lake"
+
+class LavaLakeAnnex(FusionRegion):
+    name = "Lava Lake Annex"
+
+class MotoManor(FusionRegion):
+    name = "Moto Manor"
+
+class MotoMotorway(FusionRegion):
+    name = "Moto Motorway"
+
+class NeoRidleyArena(FusionRegion):
+    name = "Neo-Ridley Arena"
+
+class NeoRidleyArenaAccess(FusionRegion):
+    name = "Neo-Ridley Arena Access"
+
+class RipperMaze(FusionRegion):
+    name = "Ripper Maze"
+
+class RipperSauna(FusionRegion):
+    name = "Ripper Sauna"
+
+class SciserPlayground(FusionRegion):
+    name = "Sciser Playground"
+
+class SciserPuddle(FusionRegion):
+    name = "Scisser Puddle"
+
+class S1EastboundGlassTube(FusionRegion):
+    name = "Sector 1 (SRX) Eastbound Glass Tube"
+
+class S1EntranceElevator(FusionRegion):
+    name = "Sector 1 (SRX) Entrance Elevator"
+
+class S1WestboundGlassTube(FusionRegion):
+    name = "Sector 1 (SRX) Westbound Glass Tube"
+
+class StabilizerCheckpoint(FusionRegion):
+    name = "Stabilizer Checkpoint"
+
+class StabilizerStorage(FusionRegion):
+    name = "Stabilizer Storage"
+
+class TourianCheckpoint(FusionRegion):
+    name = "Tourian Checkpoint"
+
+class TourianEasternHub(FusionRegion):
+    name = "Tourian Eastern Hub"
+
+class TourianElevator(FusionRegion):
+    name = "Tourian Elevator"
+
+class TourianSaveRoomEast(FusionRegion):
+    name = "Tourian Save Room East"
+
+class TourianSaveRoomWest(FusionRegion):
+    name = "Tourian Save Room West"
+
+class TourianTower(FusionRegion):
+    name = "Tourian Tower"
+
+class TourianTrapdoor(FusionRegion):
+    name = "Tourian Trapdoor"
+
+class TourianWesternHub(FusionRegion):
+    name = "Tourian Western Hub"
+
+class TwinJunctionsEast(FusionRegion):
+    name = "Twin Junctions East"
+
+class TwinJunctionsSaveRoom(FusionRegion):
+    name = "Twin Junctions Save Room"
+
+class TwinJunctionsWest(FusionRegion):
+    name = "Twin Junctions West"
+
+class WallJumpTutorial(FusionRegion):
+    name = "Wall Jump Tutorial"
+
+class WateringHole(FusionRegion):
+    name = "Watering Hole"
+
+class YamebaCorridor(FusionRegion):
+    name = "Yameba Corridor"
+
+class YamebaPool(FusionRegion):
+    name = "Yameba Pool"
+
+class ZebesianZag(FusionRegion):
+    name = "Zebesian Zag"
+
+class ZebesianZig(FusionRegion):
+    name = "Zebesian Zig"
 
 sector_1_regions = [
-    Sector1Hub,
-    Sector1TubeLeft,
-    Sector1TubeRight,
-    Sector1Antechamber,
-    Sector1FirstStabilizerZone,
-    Sector1SecondStabilizerZone,
-    Sector1ThirdStabilizerZone,
-    Sector1FourthStabilizerZone,
-    Sector1ChargeCoreZone,
-    Sector1AfterChargeCoreZone,
-    Sector1TourianExit,
-    Sector1TourianHub,
-    Sector1TourianHubElevatorTop
+    Animorphs,
+    AnimorphsCache,
+    Antechamber,
+    AtmosphericStabilizerCentral,
+    AtmosphericStabilizerNortheast,
+    AtmosphericStabilizerNorthwest,
+    AtmosphericStabilizerSoutheast,
+    AtmosphericStabilizerSouthwest,
+    CentralSaveRoom,
+    ChargeCoreArena,
+    ChargeCoreLowerAccess,
+    ChargeCoreUpperAccess,
+    CrabRave,
+    EntranceLobby,
+    EntranceNavigationRoom,
+    EntranceRechargeRoom,
+    EntranceSaveRoom,
+    GenesisHabitation,
+    GoldPirateCrossing,
+    HornoadHole,
+    HornoadHousing,
+    LavaLake,
+    LavaLakeAnnex,
+    MotoManor,
+    MotoMotorway,
+    NeoRidleyArena,
+    NeoRidleyArenaAccess,
+    RipperMaze,
+    RipperSauna,
+    SciserPlayground,
+    SciserPuddle,
+    S1EastboundGlassTube,
+    S1EntranceElevator,
+    S1WestboundGlassTube,
+    StabilizerCheckpoint,
+    StabilizerStorage,
+    TourianCheckpoint,
+    TourianEasternHub,
+    TourianElevator,
+    TourianSaveRoomEast,
+    TourianSaveRoomWest,
+    TourianTower,
+    TourianTrapdoor,
+    TourianWesternHub,
+    TwinJunctionsEast,
+    TwinJunctionsSaveRoom,
+    TwinJunctionsWest,
+    WallJumpTutorial,
+    WateringHole,
+    YamebaCorridor,
+    YamebaPool,
+    ZebesianZag,
+    ZebesianZig
 ]
