241a3ae321f7823a363743864e619d243d6f6f61
StalledStorm <112809386+StalledStorm@users.noreply.github.com>
2026-05-24 10:03:21 +0100

    Sector 2 Regions

--- worlds/metroidfusion/data/logic/regions/Sector2.py
+++ worlds/metroidfusion/data/logic/regions/Sector2.py
@@ -1,33 +1,205 @@
 from ..FusionRegion import FusionRegion
 
 
-class Sector2Hub(FusionRegion):
-    name = "Sector 2 Hub"
+class BroomCloset(FusionRegion):
+    name = "Broom Closet"
 
-class Sector2TubeLeft(FusionRegion):
-    name = "Sector 2 Tube Left"
+class Cathedral(FusionRegion):
+    name = "Cathedral"
 
-class Sector2TubeRight(FusionRegion):
-    name = "Sector 2 Tube Right"
+class CathedralCorridor(FusionRegion):
+    name = "Cathedral Corridor"
 
-class Sector2LeftSide(FusionRegion):
-    name = "Sector 2 Left Side"
+class CathedralSaveAccess(FusionRegion):
+    name = "Cathedral Save Access"
 
-class Sector2ZazabiZone(FusionRegion):
-    name = "Sector 2 Zazabi Zone"
+class CathedralSaveRoom(FusionRegion):
+    name = "Cathedral Save Room"
 
-class Sector2ZazabiZoneUpper(FusionRegion):
-    name = "Sector 2 Zazabi Zone Upper"
+class Cloister(FusionRegion):
+    name = "Cloister"
 
-class Sector2NettoriZone(FusionRegion):
-    name = "Sector 2 Nettori Zone"
+class CourtyardAccess(FusionRegion):
+    name = "Courtyard Access"
+
+class CourtyardSaveRoom(FusionRegion):
+    name = "Courtyard Save Room"
+
+class CrumbleCity(FusionRegion):
+    name = "Crumble City"
+
+class CultivationStation(FusionRegion):
+    name = "Cultivation Station"
+
+class DataCourtyard(FusionRegion):
+    name = "Data Courtyard"
+
+class DataRoom(FusionRegion):
+    name = "Data Room"
+
+class DessgeegaDorm(FusionRegion):
+    name = "Dessgeega Dorm"
+
+class EntranceLobby(FusionRegion):
+    name = "Entrance Lobby"
+
+class EntranceNavigationRoom(FusionRegion):
+    name = "Entrance Navigation Room"
+
+class EntranceRechargeRoom(FusionRegion):
+    name = "Entrance Recharge Room"
+
+class EntranceSaveRoom(FusionRegion):
+    name = "Entrance Save Room"
+
+class HiddenRechargeRoom(FusionRegion):
+    name = "Hidden Recharge Room"
+
+class KagoRoom(FusionRegion):
+    name = "Kago Room"
+
+class KihunterHallway(FusionRegion):
+    name = "Kihunter Hallway"
+
+class Level1SecurityRoom(FusionRegion):
+    name = "Level 1 Security Room"
+
+class LobbyCache(FusionRegion):
+    name = "Lobby Cache"
+
+class MaintenanceWing(FusionRegion):
+    name = "Maintenance Wing"
+
+class NettoriArena(FusionRegion):
+    name = "Nettori Arena"
+
+class NettoriArenaAccess(FusionRegion):
+    name = "Nettori Arena Access"
+
+class NettoriSaveRoom(FusionRegion):
+    name = "Nettori Save Room"
+
+class Oasis(FusionRegion):
+    name = "Oasis"
+
+class OasisStorage(FusionRegion):
+    name = "Oasis Storage"
+
+class OvergrownCache(FusionRegion):
+    name = "Overgrown Cache"
+
+class OvergrownCheckpoint(FusionRegion):
+    name = "Overgrown Checkpoint"
+
+class OvergrownEntrance(FusionRegion):
+    name = "Overgrown Entrance"
+
+class OvergrownHallway(FusionRegion):
+    name = "Overgrown Hallway"
+
+class OvergrownSpire(FusionRegion):
+    name = "Overgrown Spire"
+
+class OwtchOffice(FusionRegion):
+    name = "Owtch Office"
+
+class PuyoCorridor(FusionRegion):
+    name = "Puyo Corridor"
+
+class PuyoPalace(FusionRegion):
+    name = "Puyo Palace"
+
+class ReoRoom(FusionRegion):
+    name = "Reo Room"
+
+class RipperTower(FusionRegion):
+    name = "Ripper Tower"
+
+class Sanctum(FusionRegion):
+    name = "Sanctum"
+
+class Sector2EastboundGlassTube(FusionRegion):
+    name = "Sector 2 (TRO) Eastbound Glass Tube"
+
+class Sector2EntranceElevator(FusionRegion):
+    name = "Sector 2 (TRO) Entrance Elevator"
+
+class Sector2WestboundGlassTube(FusionRegion):
+    name = "Sector 2 (TRO) Westbound Glass Tube"
+
+class SecurityAccess(FusionRegion):
+    name = "Security Access"
+
+class SecurityCorridor(FusionRegion):
+    name = "Security Corridor"
+
+class ShootingGallery(FusionRegion):
+    name = "Shooting Gallery"
+
+class ZazabiArena(FusionRegion):
+    name = "Zazabi Arena"
+
+class ZazabiArenaAccess(FusionRegion):
+    name = "Zazabi Arena Access"
+
+class ZazabiSaveRoom(FusionRegion):
+    name = "Zazabi Save Room"
+
+class ZazabiSpeedway(FusionRegion):
+    name = "Zazabi Speedway"
+
+class ZigZagZone(FusionRegion):
+    name = "Zig-Zag-Zone"
 
 sector_2_regions = [
-    Sector2Hub,
-    Sector2TubeLeft,
-    Sector2TubeRight,
-    Sector2LeftSide,
-    Sector2ZazabiZone,
-    Sector2ZazabiZoneUpper,
-    Sector2NettoriZone
+    BroomCloset,
+    Cathedral,
+    CathedralCorridor,
+    CathedralSaveAccess,
+    CathedralSaveRoom,
+    Cloister,
+    CourtyardAccess,
+    CourtyardSaveRoom,
+    CrumbleCity,
+    CultivationStation,
+    DataCourtyard,
+    DataRoom,
+    DessgeegaDorm,
+    EntranceLobby,
+    EntranceNavigationRoom,
+    EntranceRechargeRoom,
+    EntranceSaveRoom,
+    HiddenRechargeRoom,
+    KagoRoom,
+    KihunterHallway,
+    Level1SecurityRoom,
+    LobbyCache,
+    MaintenanceWing,
+    NettoriArena,
+    NettoriArenaAccess,
+    NettoriSaveRoom,
+    Oasis,
+    OasisStorage,
+    OvergrownCache,
+    OvergrownCheckpoint,
+    OvergrownEntrance,
+    OvergrownHallway,
+    OvergrownSpire,
+    OwtchOffice,
+    PuyoCorridor,
+    PuyoPalace,
+    ReoRoom,
+    RipperTower,
+    Sanctum,
+    Sector2EastboundGlassTube,
+    Sector2EntranceElevator,
+    Sector2WestboundGlassTube,
+    SecurityAccess,
+    SecurityCorridor,
+    ShootingGallery,
+    ZazabiArena,
+    ZazabiArenaAccess,
+    ZazabiSaveRoom,
+    ZazabiSpeedway,
+    ZigZagZone,
 ]
